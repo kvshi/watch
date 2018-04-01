1dbd8c91208de84094f76d937ac547ec0e03efa7
alx-sdv <30608516+alx-sdv@users.noreply.github.com>
2018-04-01 15:28:52 +0300

    it wasn't obvious

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(name='watch'
       , url='https://github.com/alx-sdv/watch'
       , license='MIT'
       , python_requires='>=3.6.0'
+      , include_package_data=True
       , packages=['watch']
       , package_data={'watch': ['static/*', 'templates/*']}
       , install_requires=['cx-Oracle>=6.0.3', 'Flask>=0.12.2', 'pygal>=2.4.0'])
